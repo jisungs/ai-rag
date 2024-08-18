6828a397ae34ea1ec37612954d3e69b4d5dc2082
Jisung Smart <jisung.smart@gmail.com>
2024-08-19 04:43:44 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from llama_index.llms.ollama import Ollama
+from llama_parse import LlamaParse
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
+from llama_index.core.embeddings import resolve_embed_model
+
+llm = Ollama(model='llama3:8b', request_timeout=30.0)
+
