3a99e41cc6a7da6164a7f4a74acd6aca6b9066b2
Jisung Smart <jisung.smart@gmail.com>
2024-08-19 05:54:59 +0900

    download codellama and answer the question complete

--- main.py
+++ main.py
@@ -2,6 +2,50 @@ from llama_index.llms.ollama import Ollama
 from llama_parse import LlamaParse
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
 from llama_index.core.embeddings import resolve_embed_model
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.core.agent import ReActAgent
 
-llm = Ollama(model='llama3:8b', request_timeout=30.0)
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core import Settings
 
+from prompts import context
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = Ollama(model='llama3:8b', request_timeout=3600.0)
+
+parser = LlamaParse(result_type="markdown")
+
+file_extractor = {".pdf":parser}
+documents = SimpleDirectoryReader("./data", file_extractor=file_extractor).load_data()
+
+Settings.embed_model = HuggingFaceEmbedding(
+    model_name="BAAI/bge-small-en-v1.5"
+)
+embed_model = resolve_embed_model("local:BAAI/bge-m3")
+vector_index = VectorStoreIndex.from_documents(documents, embede_model=embed_model)
+query_engine = vector_index.as_query_engine(llm=llm)
+
+# query_engine.query("")
+# result = query_engine.query("what are some of the routes in the api?")
+# print(result)
+
+tools = [
+    QueryEngineTool(
+        query_engine=query_engine,
+        metadata=ToolMetadata(
+            name = "flask_api_docs",
+            description = "This gives documentation about code for an api"
+        )
+    )
+]
+
+code_llm = Ollama(model="codellama")
+agent = ReActAgent.from_tools(tools, llm=code_llm, verbose=True, context=context)
+
+while (prompt := input("Enter a prompt (q to quit): ")) != "q":
+    result = agent.query(prompt)
+    print(result)
